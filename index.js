ceb3be5283243b7287da0f28fcaef190c582df4b
Akshat Jain <jain.akshat120@gmail.com>
2022-02-17 12:24:53 +0530

    Set Partials and Layouts with static files in project

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const port = 8000;
 const app = express();
+const expressLayouts = require('express-ejs-layouts');
+
+app.use(expressLayouts);
+app.use(express.urlencoded());
+
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+
 // use express router
 app.use('/', require('./routes'));
+
+app.use(express.static('./assets'));
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 app.listen(port, function(err){
     if(err){
